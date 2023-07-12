40bd7a6891d7c8a4d187b736b9ea74317b53cb28
qzw <w_qinzhiwei@xiwang.com>
2023-07-12 11:45:47 +0800

    test: 不能开头 Sentence case

--- commitlint.config.js
+++ commitlint.config.js
@@ -2,7 +2,7 @@
  * @Author: qzw w_qinzhiwei@xiwang.com
  * @Date: 2023-07-12 11:17:40
  * @LastEditors: qzw w_qinzhiwei@xiwang.com
- * @LastEditTime: 2023-07-12 11:41:48
+ * @LastEditTime: 2023-07-12 11:44:50
  * @FilePath: /changelog-demo-1/commitlint.config.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -17,16 +17,7 @@ module.exports = {
         'subject-case': [
             2,
             'never',
-            [
-                'lower-case', // default
-                'upper-case', // UPPERCASE
-                'camel-case', // camelCase
-                'kebab-case', // kebab-case
-                'pascal-case', // PascalCase
-                'sentence-case', // Sentence case
-                'snake-case', // snake_case
-                'start-case', // Start Case
-            ],
+            ['sentence-case', 'start-case', 'pascal-case', 'upper-case'],
         ],
         'subject-empty': [2, 'never'],
         'subject-full-stop': [2, 'never', '.'],
