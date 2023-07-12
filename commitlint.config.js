1d8edebafd7e8ce42784ea5074e20b996c2d650d
qzw <w_qinzhiwei@xiwang.com>
2023-07-12 11:41:57 +0800

    test: 校验规则 default

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,52 @@
-module.exports = {extends: ['@commitlint/config-conventional']}
+/*
+ * @Author: qzw w_qinzhiwei@xiwang.com
+ * @Date: 2023-07-12 11:17:40
+ * @LastEditors: qzw w_qinzhiwei@xiwang.com
+ * @LastEditTime: 2023-07-12 11:41:48
+ * @FilePath: /changelog-demo-1/commitlint.config.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+module.exports = {
+    extends: ['@commitlint/config-conventional'],
+    rules: {
+        'body-leading-blank': [1, 'always'],
+        'body-max-line-length': [2, 'always', 100],
+        'footer-leading-blank': [1, 'always'],
+        'footer-max-line-length': [2, 'always', 100],
+        'header-max-length': [2, 'always', 100],
+        'subject-case': [
+            2,
+            'never',
+            ['lower-case', // default
+                'upper-case', // UPPERCASE
+                'camel-case', // camelCase
+                'kebab-case', // kebab-case
+                'pascal-case', // PascalCase
+                'sentence-case', // Sentence case
+                'snake-case', // snake_case
+                'start-case', // Start Case
+            ],
+        ],
+        'subject-empty': [2, 'never'],
+        'subject-full-stop': [2, 'never', '.'],
+        'type-case': [2, 'always', 'lower-case'],
+        'type-empty': [2, 'never'],
+        'type-enum': [
+            2,
+            'always',
+            [
+                'build',
+                'chore',
+                'ci',
+                'docs',
+                'feat',
+                'fix',
+                'perf',
+                'refactor',
+                'revert',
+                'style',
+                'test',
+            ],
+        ],
+    }
+}
